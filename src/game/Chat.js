e752323a6121ca453c5b38603b9260ad899e4b42
Rolando Islas <admin@rolandoislas.com>
2018-02-03 10:07:51 -0700

    Handle letter coordinates and various syntax of the command

--- src/game/Chat.js
+++ src/game/Chat.js
@@ -1,41 +1,54 @@
 var Chat = function(logic) {
 	this.logic = logic;
 	this.player;
+	this.commandPlaceTile = new RegExp("^([\\d{1,2}a-zA-Z])[-\\s]?(\\d{1,2})\\s?([a-zA-Z])$");
+	this.commandRemovetile = new RegExp("^(?:remove|del|rem|delete)\\s([\\d{1,2}a-zA-Z])[-\\s]?(\\d{1,2})$", "i");
 };
 
+function letterToNumber(letter) {
+	var number = parseInt(letter);
+	if (!isNaN(number))
+		return number;
+    var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    return letters.indexOf(letter.toUpperCase())
+}
+
 Chat.prototype.parseMessage = function(user, message, callback) {
-	if (/^\d{1,2}[-\s]\d{1,2}\s\w$/.test(message)) { // "(int)-(int) (char)" position tile
-		if (message.indexOf("-") < 0)
-			message = message.replace(" ", "-");
-		var letter = message.split(" ")[1];
-		if (letter.length != 1 || !(/[a-z]/i.test(letter)))
-			return callback("Invalid letter.");
-		var x = parseInt(message.split(" ")[0].split("-")[0]);
-		var y = parseInt(message.split(" ")[0].split("-")[1]);
-		if (x > 14 || y < 0)
-			return callback("Coordinates must range from 0 to 14.");
-		this.logic.submitTilePosition(this.player, x, y, letter, user.username, function(err) {
+	if (this.commandPlaceTile.test(message)) { // "(int/char)-(int) (char)" position tile
+		var groups = this.commandPlaceTile.exec(message);
+		var x = parseInt(letterToNumber(groups[1]));
+		var y = parseInt(groups[2]);
+		if (x > 14 || x < 0 || y > 14 || y < 0)
+			return callback("Coordinates must range from 0 to 14 or A to O.");
+		this.logic.submitTilePosition(this.player, x, y, groups[3], user.username, function(err) {
 			return callback(err);
 		});
-	} else if (/^remove\s\d{1,2}-\d{1,2}$/i.test(message)) { // "remove (int)-(int)" remove tile
-		var x = parseInt(message.split(" ")[1].split("-")[0]);
-		var y = parseInt(message.split(" ")[1].split("-")[1]);
+	}
+	else if (this.commandRemovetile.test(message)) { // "remove/rem/del/delete (int/char)-(int)" remove tile
+        var groups = this.commandRemovetile.exec(message);
+		var x = parseInt(letterToNumber(groups[1]));
+		var y = parseInt(groups[2]);
+        if (x > 14 || x < 0 || y > 14 || y < 0)
+            return callback("Coordinates must range from 0 to 14 or A to O.");
 		this.logic.removeTile(this.player, x, y, user.username, function(err) {
 			return callback(err);
 		});
-	} else if (/^submit$/i.test(message)) { // submit turn
+	}
+	else if (/^submit$/i.test(message)) { // submit turn
 		this.logic.submitTurn(this.player, user.username, function(err) {
 			return callback(err);
 		});
-	} else if (/^swap\s\w$/i.test(message)) { // swap tile
+	}
+	else if (/^swap\s([a-zA-Z])$/i.test(message)) { // swap tile
 		var letter = message.split(" ")[1];
 		if (letter.length != 1 || !(/[a-z]/i.test(letter)))
 			return callback("Invalid letter.");
 		this.logic.swapTile(this.player, letter, user.username, function(err) {
 			return callback(err);
 		});
-	} else if (/^[a-z]{1}$/i.test(message)) { // single letter
-		return callback("To play a letter you must enter coordinates. Ex. 0-7 " + message);
+	}
+	else if (/^([a-zA-Z])$/.test(message)) { // single letter
+		return callback("To play a letter you must enter coordinates. Ex. a-7 " + message);
 	}
 };
 
